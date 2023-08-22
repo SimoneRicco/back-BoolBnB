1cb43f2b2d68a3c69e046b384e9f572bcc935f06
SimoneRicco <smnrcc0@gmail.com>
2023-08-22 17:03:31 +0200

    a caso

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -2,7 +2,7 @@
     <header>
         <h2 class="text-lg font-medium text-gray-900">
             {{ __('Delete Account') }}
-        </h2>
+        </h2> 
 
         <p class="mt-1 text-sm text-gray-600">
             {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
