1f778333eafe84cb5114c10c44910d6f6bb9271a
AlessioAbbati <alex_117@live.it>
2023-08-25 18:34:47 +0200

    add api route and apicontroller

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,18 @@
 <?php
 
 use App\Http\Controllers\Api\ApartmentController;
+use App\Http\Controllers\Api\ImageController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\AddressController;
+use App\Http\Controllers\Api\viewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 Route::get('apartments', [ApartmentController::class, 'index'])->name('api.apartments.index'); // {nomeSito}/api/apartments
 Route::get('apartments/{apartment}', [ApartmentController::class, 'show'])->name('api.apartments.show');
+Route::get('users/', [UserController::class, 'index'])->name('api.users.index');
+Route::get('images', [ImageController::class, 'index'])->name('api.images.index');
+Route::get('addresses', [AddressController::class, 'index'])->name('api.addresses.index');
+Route::get('views', [ViewController::class, 'index'])->name('api.views.index');
