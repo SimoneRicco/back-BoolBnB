cf46b607d119fb82a4a54e5f857d01f52f6bb2f9
AlessioAbbati <alex_117@live.it>
2023-08-26 14:06:24 +0200

    mod migrations and print images in apartment

--- config/images.php
+++ config/images.php
@@ -116,27 +116,27 @@ return [
         'url'          => "01appartamentoaduepassidall'arno.jpg",
         'cover_image'  => true,
 
-        'apartment_id' => '1',
+        'apartment_id' => '5',
     ],
     [
         'name'         => 'appartamento a due passi dall\'arno',
         'url'          => "02appartamentoaduepassidall'arno.jpg",
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '5',
 
     ],
     [
         'name'         => 'appartamento a due passi dall\'arno',
         'url'          => "03appartamentoaduepassidall'arno.jpg",
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '5',
 
     ],
     [
         'name'         => 'appartamento a due passi dall\'arno',
         'url'          => "04appartamentoaduepassidall'arno.jpg",
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '5',
 
     ],
 
@@ -144,27 +144,27 @@ return [
         'name'         => 'bilocale luminoso',
         'url'          => '01bilocaleluminoso.jpg',
         'cover_image'  => true,
-        'apartment_id' => '1',
+        'apartment_id' => '6',
     ],
     [
         'name'         => 'bilocale luminoso',
         'url'          => '02bilocaleluminoso.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '6',
 
     ],
     [
         'name'         => 'bilocale luminoso',
         'url'          => '03bilocaleluminoso.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '6',
 
     ],
     [
         'name'         => 'bilocale luminoso',
         'url'          => '04bilocaleluminoso.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '6',
 
     ],
 
@@ -172,27 +172,27 @@ return [
         'name'         => 'monolocale per studenti',
         'url'          => '01monolocaleperstudenti.jpg',
         'cover_image'  => true,
-        'apartment_id' => '1',
+        'apartment_id' => '7',
     ],
     [
         'name'         => 'monolocale per studenti',
         'url'          => '02monolocaleperstudenti.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '7',
 
     ],
     [
         'name'         => 'monolocale per studenti',
         'url'          => '03monolocaleperstudenti.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '7',
 
     ],
     [
         'name'         => 'monolocale per studenti',
         'url'          => '04monolocaleperstudenti.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '7',
 
     ],
 
@@ -200,27 +200,27 @@ return [
         'name'         => 'Baita sul Fiume',
         'url'          => '01BaitasulFiume.jpg',
         'cover_image'  => true,
-        'apartment_id' => '1',
+        'apartment_id' => '8',
     ],
     [
         'name'         => 'Baita sul Fiume',
         'url'          => '02BaitasulFiume.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '8',
 
     ],
     [
         'name'         => 'Baita sul Fiume',
         'url'          => '03BaitasulFiume.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '8',
 
     ],
     [
         'name'         => 'Baita sul Fiume',
         'url'          => '04BaitasulFiume.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '8',
 
     ],
 
@@ -228,27 +228,27 @@ return [
         'name'         => 'Cottage sul lago',
         'url'          => '01Cottagesullago.jpg',
         'cover_image'  => true,
-        'apartment_id' => '1',
+        'apartment_id' => '9',
     ],
     [
         'name'         => 'Cottage sul lago',
         'url'          => '02Cottagesullago.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '9',
 
     ],
     [
         'name'         => 'Cottage sul lago',
         'url'          => '03Cottagesullago.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '9',
 
     ],
     [
         'name'         => 'Cottage sul lago',
         'url'          => '04Cottagesullago.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '9',
 
     ],
 
@@ -256,27 +256,27 @@ return [
         'name'         => 'Loft Bella Vita',
         'url'          => '01LoftBellaVita.jpg',
         'cover_image'  => true,
-        'apartment_id' => '1',
+        'apartment_id' => '10',
     ],
     [
         'name'         => 'Loft Bella Vita',
         'url'          => '02LoftBellaVita.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '10',
 
     ],
     [
         'name'         => 'Loft Bella Vita',
         'url'          => '03LoftBellaVita.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '10',
 
     ],
     [
         'name'         => 'Loft Bella Vita',
         'url'          => '04LoftBellaVita.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '10',
 
     ],
 
@@ -284,27 +284,27 @@ return [
         'name'         => 'Casa in montagna',
         'url'          => '01Casainmontagna.jpg',
         'cover_image'  => true,
-        'apartment_id' => '1',
+        'apartment_id' => '11',
     ],
     [
         'name'         => 'Casa in montagna',
         'url'          => '02Casainmontagna.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '11',
 
     ],
     [
         'name'         => 'Casa in montagna',
         'url'          => '03Casainmontagna.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '11',
 
     ],
     [
         'name'         => 'Casa in montagna',
         'url'          => '04Casainmontagna.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '11',
 
     ],
 
@@ -312,27 +312,27 @@ return [
         'name'         => 'Casa in campagna',
         'url'          => '01Casaincampagna.jpg',
         'cover_image'  => true,
-        'apartment_id' => '1',
+        'apartment_id' => '12',
     ],
     [
         'name'         => 'Casa in campagna',
         'url'          => '02Casaincampagna.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '12',
 
     ],
     [
         'name'         => 'Casa in campagna',
         'url'          => '03Casaincampagna.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '12',
 
     ],
     [
         'name'         => 'Casa in campagna',
         'url'          => '04Casaincampagna.jpg',
         'cover_image'  => false,
-        'apartment_id' => '1',
+        'apartment_id' => '12',
 
     ],
     
